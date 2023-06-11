7c9a0cd6357a11e178e7770af46cc946aeeddf6f
IkhwanWahyudi <113562288+IkhwanWahyudi@users.noreply.github.com>
2023-06-11 15:03:33 +0800

    Add files via upload

---
+++ AplikasiAmplang/src/AplikasiAmplang/Login.java
@@ -0,0 +1,10 @@
+package AplikasiAmplang;
+
+public class Login {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            FormLogin loginForm = new FormLogin();
+            loginForm.setVisible(true);
+        });
+    }
+}
